530e103b67154ac417d59bcf51b0c7480e85c3db
Max <149390445+maxtpv@users.noreply.github.com>
2024-05-17 18:17:40 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,16 +1,14 @@
-const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'localhost:3000'
-];
+const allowedCors = ["meksfront.nomoredomainswork.ru"];
+
 function cors(req, res, next) {
-    const { origin } = req.headers;
+  const { origin } = req.headers;
 
-    if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-
-    next();
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
-module.exports = cors; //base
+module.exports = cors;
